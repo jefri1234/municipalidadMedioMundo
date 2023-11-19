ef5cfa265d984b55b3e37979ff7ea5d2393fabc0
jefri1234 <141457718+jefri1234@users.noreply.github.com>
2023-11-18 19:08:27 -0500

    primera subida

---
+++ script.js
@@ -0,0 +1,18 @@
+window.sr =ScrollReveal();
+
+    sr.reveal('.introduccion',{
+        duration:3000,
+        origin:'left',
+        distance:'-100px'
+    });
+    sr.reveal('.contenedor2',{
+        duration:3000,
+        origin:'rigth',
+        distance:'-100px'
+    });
+
+    sr.reveal('.container-mapa',{
+        duration:5000,
+        origin:'left',
+        distance:'-100px'
+    });
